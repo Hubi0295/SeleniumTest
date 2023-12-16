8601c2b7c7b98b8c757b59ceb8c3a1fcbcdb818b
= <nowa nazwa>
2023-12-16 18:11:48 +0100

    Modified Tests

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,30 +1,41 @@
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class SeleniumTest {
 
     public void OpenGooglePage(String browser){
+        browser = browser.toLowerCase();
         String address = "google.com/";
         try{
-            WebDriver driver = getDriver(browser);
+            WebDriver driver = WebDriverManagerTest.getDriver(browser);
+            driver.manage().window().maximize();
             driver.get("https://www."+address);
-            driver.close();
+//            FirefoxOptions firefoxOptions = new FirefoxOptions();
+//            firefoxOptions.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.ACCEPT);
+//            ((JavascriptExecutor)driver).executeScript("window.open('https://www.flashscore.pl/zawodnik/lewandowski-robert/MVC8zHZD/','blank','height=200','width=300')");
+//            ((JavascriptExecutor)driver).executeScript("alert('Hej')");
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            WebElement agreeButton = driver.findElement(By.xpath("//div[text()='Zaakceptuj wszystko']"));
+            agreeButton.click();
+            WebElement searchField = driver.findElement(By.name("q"));
+            searchField.sendKeys("never gonna give you up");
+            searchField.sendKeys(Keys.ENTER);
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            WebElement searchField1 = driver.findElement(By.xpath("//a[contains(@href,'youtube.com')]//h3"));
+            searchField1.click();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            WebElement result = driver.findElement(By.xpath("//button[contains(@aria-label, 'Zaakceptuj wykorzystywanie plików cookie i innych danych do opisanych celów')]"));
+            result.click();
+            Assert.assertTrue(result.isDisplayed());
+            driver.quit();
         }
         catch (WrongBrowserException e){
             System.err.println(e.getMessage());
         }
 
-
-    }
-
-    public WebDriver getDriver(String driver) throws WrongBrowserException{
-        driver = driver.toLowerCase();
-
-        return switch (driver) {
-            case "chrome" -> new Chrome();
-            case "firefox" -> new Firefox();
-            case "edge" -> new Edge();
-            default -> throw new WrongBrowserException("Wrong Browser name!");
-        };
 
     }
     @Test
